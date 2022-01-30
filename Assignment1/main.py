a70af1212f982b9c78d8f08236d8609e8d63c448
Parth Shah <phshah@uwaterloo.ca>
2022-01-29 20:00:36 -0500

    argparse

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -1,7 +1,13 @@
 # Parth Shah NLP A1
 import random
 import sys
+import argparse
 
+parser = argparse.ArgumentParser(description='Enter location for pos.tx and neg.tx')
+parser.add_argument('pos_in', type=str, help='Path to pos.txt:', default='pos.tx')
+parser.add_argument('neg_in', type=str, help='Path to neg.txt:', default='neg.tx')
+args = parser.parse_args()
 
 if __name__ == '__main__':
-    print("hello")
+    pos_path = args.pos_in
+    neg_path = args.neg_in
